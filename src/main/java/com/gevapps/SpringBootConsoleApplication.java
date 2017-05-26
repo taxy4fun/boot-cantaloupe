71b234955fe08fa380bffd5bb8e564cd79930a1a
MFMolaGit <manuelf.mola@gmail.com>
2017-05-26 19:02:54 +0200

    Initial commit

---
+++ src/main/java/com/gevapps/SpringBootConsoleApplication.java
@@ -0,0 +1,21 @@
+package com.gevapps;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsoleApplication implements CommandLineRunner {
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(SpringBootConsoleApplication.class, args);
+
+    }
+
+    //access command line arguments
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!");
+    }
+}
