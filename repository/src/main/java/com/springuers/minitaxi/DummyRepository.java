9231e6272a549dbadd47fbcf887ccd81e561d3e1
butcherless <cmarting@gmail.com>
2017-06-10 12:11:22 +0200

    infraestructura básica para proyecto multi-module

---
+++ repository/src/main/java/com/springuers/minitaxi/DummyRepository.java
@@ -0,0 +1,11 @@
+package com.springuers.minitaxi;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by cmartin on 10/06/2017.
+ */
+@Repository
+public class DummyRepository {
+
+}
