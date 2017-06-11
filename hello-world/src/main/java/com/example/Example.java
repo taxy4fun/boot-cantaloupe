80660b4a8646393b992ef39a56c236ec33b1d146
jperezdelafuente <jesusperez82@gmail.com>
2017-06-11 13:44:08 +0200

    Se añade soporte a web

--- hello-world/src/main/java/com/example/Example.java
+++ hello-world/src/main/java/com/example/Example.java
@@ -5,7 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class Example implements CommandLineRunner {
 
@@ -15,6 +19,12 @@ public class Example implements CommandLineRunner {
 
     public Example(FooProperties properties) {
         this.properties = properties;
+    }
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
     }
 
     @Override
