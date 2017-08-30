d87a0a60c61d7ce200e6a5dc0e4546e622c20e37
butcherless <cmarting@gmail.com>
2017-08-30 19:15:01 +0200

    polishing rubbish

---
+++ hello-world/src/main/java/com/springuers/taxy4fun/HelloWorldApp.java
@@ -0,0 +1,23 @@
+package com.springuers.taxy4fun;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApp implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorldApp.class);
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("Hello from Boot Cantaloupe basic application");
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorldApp.class, args);
+    }
+
+}
